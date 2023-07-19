341a4c5aedf6ec5e5a0e6599c58c14bec262df38
zl <zl_chen@asus.com>
2023-07-19 14:04:14 +0800

    notification.py

--- notification.py
+++ notification.py
@@ -7,6 +7,7 @@ Created on 2023/07/18
 '''
 
 import os, requests, pymsteams, configparser
+# import os, configparser, sys
 
 config = configparser.ConfigParser()
 config.read('notification.ini')
@@ -15,29 +16,25 @@ class notify_by_pymsteams(object):
 	def __init__(self):
 		pass
 	def main(self):
-		self.teams()
-	def teams(self):
+		self.teams_post()
+	def teams_post(self):
 		webhook_url = config.get('setting', 'webhook_url')
-		message = {
-			'text': 'Gerrit',
-			'title': 'Notification',
-		}
 		content = pymsteams.connectorcard(webhook_url)
-		content.title("This is my message title")
-		content.text('ZL')
-		content.addLinkButton("This is the button Text", "https://github.com/rveachkc/pymsteams/")
+		content.title('Code-Review + 1')
+		content.text('oe22_obmc_review - 23')
+		content.addLinkButton('oe22_obmc_review - 23', 'http://10.10.95.52:8088/job/oe22_obmc_review/23/')
 		content.send()
 
 class notify_by_requests(object):
 	def __init__(self):
 		pass
 	def main(self):
-		self.teams()
-	def teams(self):
+		self.teams_post()
+	def teams_post(self):
 		webhook_url = config.get('setting', 'webhook_url')
 		message = {
-			'text': 'Gerrit',
-			'title': 'Notification',
+			'title': 'Code-Review + 1',
+			'text': 'http://10.10.95.52:8088/job/oe22_obmc_review/23/',
 		}
 		response = requests.post(webhook_url, json=message)
 		if response.status_code == 200:
@@ -49,20 +46,21 @@ class notify_by_curl(object):
 	def __init__(self):
 		pass
 	def main(self):
-		self.teams()
-	def teams(self):
+		self.teams_post()
+	def teams_post(self):
 		webhook_url = config.get('setting', 'webhook_url')
 		message = {
-			'text': 'Gerrit',
-			'title': 'Notification',
+			'title': 'Code-Review + 1',
+			'text': 'http://10.10.95.52:8088/job/oe22_obmc_review/23/',
 		}
-		content = 'curl -H \"Content-Type:application/json\" -d \"' + str(message) + '\" ' + webhook_url
-		os.system(content)
+		content_post = 'curl -H \"Content-Type:application/json\" -d \"' + str(message) + '\" ' + webhook_url
+		print(content_post)
+		os.system(content_post)
 
 if __name__ == '__main__':
 	n = notify_by_pymsteams()
-	n.main()
+	n.main() # type: ignore
 	n = notify_by_requests()
-	n.main()
+	n.main() # type: ignore
 	n = notify_by_curl()
-	n.main()
+	n.main() # type: ignore
